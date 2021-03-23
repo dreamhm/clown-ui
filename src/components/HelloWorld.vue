0d200470a3e8f0aa8c95f02b2a0f6f723e3e193d
“hm <1425178814@qq.com>
2021-03-23 17:13:27 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>　
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'I LOVE YOU, NIU NA!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: #36281b73;
+}
+</style>
