c41d9523aed41d6f696883b39a99d9271e1dd8ee
“hm <1425178814@qq.com>
2021-03-25 18:54:05 +0800

    login 部分代码编写

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+/* Layout */
+import Layout from '@/layout'
+import ParentView from '@/components/ParentView'
+
+/**
+ * Note: 路由配置项
+ *
+ * hidden: true 
+ * alwaysShow: true
+ * 
+ * 
+ * 
+ * redirect: noRedirect
+ * name: 'router-name'
+ * meta: {
+		noCache: true
+		title: 'title'
+		icon: 'svg-name'
+		breadcrumb: false
+  }
+ */
+
+// 公共路由
+export const constantRoutes = [
+	{
+		path: '/redirect',
+		component: Layout,
+		hidden: true,
+		children: [
+			{
+				path: '/redirect/:path(.*)',
+				component: (resolve) => require(['@/views/redirect'], resolve)
+			}
+		]
+	},
+	{
+		path: '/login',
+		component: (resolve) => require(['@/views/login'], resolve),
+		hidden: true
+	}
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+	mode: 'history', // 去掉url中的#
+	scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
